1bb54ad85ec028ba9b51918bdcb81c04edf5d916
Vishakh Sandwar <vishakhs74@gmail.com>
2024-04-14 11:57:12 -0400

    new images

--- pages/3_CircuitSoln.py
+++ pages/3_CircuitSoln.py
@@ -4,14 +4,17 @@ st.markdown(
     """
     #Here's the default circuit we were given:
 
-    ![Default Circuit](../../default_circuit.png)
+    ![Default Circuit](https://i.ibb.co/fFt6138/Screenshot-2024-04-14-at-9-57-23-AM.png)
+
+    HERE!
+
 
     # And here's the implementation we designed:
 
     
     Note the use of adders in this circuit due to the powerful nature of the 
     QFT adder which scales linearly in depth with the number of qubits.
-    ![Adder Table](../../adders.png)
-    
+    ![Adder Table](https://ibb.co/hRMfGm5)
+
     """
 )
